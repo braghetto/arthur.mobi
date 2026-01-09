54c949174809ab7d57d76d861af74ebbfe1160d4
Arthur Mochiuti Braghetto <arthurmb@gmail.com>
2026-01-09 16:48:30 -0300

    Remove tina

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '0ad4dd29012cdf38d54b06ba6eaeee5a9931122b', queries });
-export default client;
-  
